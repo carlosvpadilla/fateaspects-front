65fbbe0bbf7bb428544211d0e7b61e92d7b82a3c
Carlos Vergara <carlos.vpadilla@gmail.com>
2019-03-27 20:54:57 -0300

    Add default route to Express

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 let app = express();
 app.use(express.static(path.join(__dirname, 'index.html')));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
